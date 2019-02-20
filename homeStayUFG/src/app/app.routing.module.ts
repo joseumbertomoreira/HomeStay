5a20c13a286d0672196980e1393dca139751653b
jose <jose@localhost.localdomain>
2019-02-19 22:57:53 -0300

    host-profile

--- homeStayUFG/src/app/app.routing.module.ts
+++ homeStayUFG/src/app/app.routing.module.ts
@@ -6,7 +6,7 @@ import { HostProfileComponent } from "./host-profile/host-profile.component";
 
 const routes: Routes = [
     { path: '', component: CardComponent },
-    { path: 'user/flavio', component: HostProfileComponent },
+    { path: 'user/host', component: HostProfileComponent },
 ];
 
 @NgModule({
